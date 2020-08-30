80d1ba09f32f110eb952edbb63f9fdb17750e9c0
Pete VS <pete_vs88@me.com>
2020-08-30 19:58:28 -0400

    completed task 4

--- index.js
+++ index.js
@@ -257,9 +257,17 @@ console.log(getArtistByIndex(artists, 0));
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */) {
-  /* Code here */
+function get20s(array) {
+  let artistsNarrowed = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].years[1] === "9" && array[i].years[7] !== "2") {
+      artistsNarrowed.push(array[i]);
+    }
+  }
+  return artistsNarrowed;
 }
+
+console.log(get20s(artists));
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
